f0aab3b96dec97f68afbbfa3422d618065dc21ff
voroshkov <voroshkov@gmail.com>
2015-12-26 17:40:10 +0300

    improved sticks sharpness and resolution

--- rc-leonardo-joy.ino
+++ rc-leonardo-joy.ino
@@ -1,182 +1,206 @@
+/**************************************************************************************************
+This sketch turns Arduino Leonardo (or Pro Micro) into a USB adapter for RC transmitter.
+The adapter can be used to play FPV Freerider (http://fpv-freerider.itch.io/fpv-freerider) 
+or other flight simulators. 
 
-#include "UnoJoy.h"
+Prerequisite: 
+Put "hid.cpp" and "usbapi.h" from ArduinoLibs folder into Arduino installation folder before compiling:
+	<Arduino_Installation_Folder>\hardware\arduino\cores\arduino\ 
 
-boolean usePPM = true;
+Connections:
+- RC PPM out <--> Digital Pin 4 of Arduino Leonardo
+- RC GND  <--> Arduino GND
 
-#define RC_CHANS 5
-volatile uint16_t rcValue[RC_CHANS] = {1502, 1502, 1502, 1502, 1502};
+Based on https://github.com/i--storm/rc-leonardo-joy , but enhanced as follows:
+- improved sticks resolution (from 255 to 1000),
+- improved reading of ppm intervals (using timer input capture interrupt instead of micros() func)
+- cleaned up code 
 
+Done by Andrey Voroshkov in December 2015, 
+- github:	https://github.com/voroshkov
+- youtube:	https://www.youtube.com/user/voroshkov 
+- facebook: https://www.facebook.com/andrey.voroshkov
+- blogspot:	http://voroshkov.blogspot.com
+**************************************************************************************************/
+
+#include "Arduino.h"
+#include <avr/interrupt.h>
+
+// Use for Futaba transmitters (they have shifted center value and narrower range by default)
+#define FUTABA
+
+// Use to enable output of PPM values to serial
+// #define SERIALOUT
+
+#define RC_CHANNELS_COUNT 6
+
+#ifdef FUTABA
+	#define STICK_HALFWAY 450
+	#define STICK_CENTER 1530
+	#define THRESHOLD 200
+#else
+	#define STICK_HALFWAY 500
+	#define STICK_CENTER 1500
+	#define THRESHOLD 100 // threshold is used to detect PPM values (added to range at both ends)
+#endif
+
+#define USB_STICK_VALUE_MAX 1000
+
+#define MIN_PULSE_WIDTH (STICK_CENTER - STICK_HALFWAY)
+#define MAX_PULSE_WIDTH (STICK_CENTER + STICK_HALFWAY)
+#define NEWFRAME_PULSE_WIDTH 3000
+
+// timer capture ICP1 pin corresponds to Leonardo digital pin 4
+#define PPM_CAPTURE_PIN 4
 #define LED_PIN 13
-byte led = 1;
-int led_cnt = 0;
 
-const bool testAutoSendMode = false;
+// for timer prescaler set to 1/8 of 16MHz, counter values should be
+//  divided by 2 to get the number of microseconds
+#define TIMER_COUNT_DIVIDER 2 
 
+// this array contains the lengths of read PPM pulses in microseconds
+volatile uint16_t rcValue[RC_CHANNELS_COUNT];
+
+// enum defines the order of channels
 enum {
-  ROLL,
-  PITCH,  
-  THROTTLE,
-  YAW,
-  AUX1
+	ROLL,
+	PITCH,  
+	THROTTLE,
+	YAW,
+	AUX1,
+	AUX2
 };
 
 void setup() {
-  setupPins();
-  if (testAutoSendMode) {
-    Joystick.begin();
-  } else {
-    Joystick.begin(false);
-  }
+	setupPins();
+	initTimer();
+	Joystick.begin(false);
+
+#ifdef SERIALOUT
+	Serial.begin(115000);
+#endif
 }
 
 void loop(){
-  // Always be getting fresh data
-  dataForController_t controllerData = getControllerData();
-  setControllerDataJoystick(controllerData);
-  Joystick.sendState();
+	// dataForController_t controllerData = getControllerData();
+	setControllerDataJoystick();
+	Joystick.sendState();
 
-  // Serial.print(controllerData.rightStickX); 
-  // Serial.print("\t");
-  // Serial.print(controllerData.rightStickY);
-  // Serial.print("\t");
-  // Serial.print(controllerData.leftStickX);
-  // Serial.print("\t");
-  // Serial.print(controllerData.leftStickY);
-  // Serial.println("\t");
-
-  // led_cnt=led_cnt++;
-  // if(led_cnt>200){
-  //   led=!led;
-  //   digitalWrite(LED_PIN,led);
-  //   led_cnt=0;
-  // }
-  
-  // delay(5);
+#ifdef SERIALOUT	
+	Serial.print(rcValue[0]); 
+	Serial.print("\t");
+	Serial.print(rcValue[1]); 
+	Serial.print("\t");
+	Serial.print(rcValue[2]); 
+	Serial.print("\t");
+	Serial.print(rcValue[3]); 
+	Serial.print("\t");
+	Serial.print(rcValue[4]); 
+	Serial.print("\t");
+	Serial.print(rcValue[5]); 
+	Serial.print("\t");
+	Serial.print(controllerData.rightStickX); 
+	Serial.print("\t");
+	Serial.print(controllerData.rightStickY);
+	Serial.print("\t");
+	Serial.print(controllerData.leftStickX);
+	Serial.print("\t");
+	Serial.print(controllerData.leftStickY);
+	Serial.print("\t");
+	Serial.print(controllerData.triangleOn);
+	Serial.print("\t");
+	Serial.print(controllerData.circleOn);
+	Serial.println("\t");
+#endif
 }
 
-void setControllerDataJoystick(dataForController_t dataToSet){
-  Joystick.setXAxis(dataToSet.leftStickX);
-  Joystick.setYAxis(dataToSet.leftStickY);
-  Joystick.setXAxisRotation(dataToSet.rightStickX);
-  Joystick.setYAxisRotation(dataToSet.rightStickY);
+void initTimer(void) { 
+	// Input Capture setup
+	// ICNC1: =0 Disable Input Capture Noise Canceler to prevent delay in reading
+	// ICES1: =1 for trigger on rising edge
+	// CS11: =1 set prescaler to 1/8 system clock (F_CPU)
+	TCCR1A = 0;
+	TCCR1B = (0<<ICNC1) | (1<<ICES1) | (1<<CS11);
+	TCCR1C = 0;
+
+	// Interrupt setup
+	// ICIE1: Input capture 
+	TIFR1 = (1<<ICF1); // clear pending
+	TIMSK1 = (1<<ICIE1); // and enable
 }
 
 void setupPins(void){
-  // Set all the digital pins as inputs
-  // with the pull-up enabled, except for the 
-  // two serial line pins
+	// Set up the Input Capture pin
+	pinMode(PPM_CAPTURE_PIN, INPUT);
+	digitalWrite(PPM_CAPTURE_PIN, 1); // enable the pullup
 
-  // for (int i = 2; i <= 12; i++){
-  //   pinMode(i, INPUT);
-  //   digitalWrite(i, HIGH);
-  // }
-
-  pinMode(A4, INPUT);
-  digitalWrite(A4, HIGH);
-  pinMode(A5, INPUT);
-  digitalWrite(A5, HIGH);
-  
-  if ( usePPM ) { 
-    attachInterrupt(0, rxInt, RISING); //interrupt 0 is pin#3 on Leonardo
-  }
-  
-  pinMode(LED_PIN, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
 }
 
-dataForController_t getControllerData(void){
-  
-  // Set up a place for our controller data
-  //  Use the getBlankDataForController() function, since
-  //  just declaring a fresh dataForController_t tends
-  //  to get you one filled with junk from other, random
-  //  values that were in those memory locations before
-  dataForController_t controllerData = getBlankDataForController();
-  // Since our buttons are all held high and
-  //  pulled low when pressed, we use the "!"
-  //  operator to invert the readings from the pins
-
-  // if ( !usePPM ) {
-  //   controllerData.triangleOn = !digitalRead(2); //pin 2 is used for PPM input
-  // }
-
-  // controllerData.circleOn = !digitalRead(3);
-  // controllerData.squareOn = !digitalRead(4);
-  // controllerData.crossOn = !digitalRead(5);
-  // controllerData.dpadUpOn = !digitalRead(6);
-  // controllerData.dpadDownOn = !digitalRead(7);
-  // controllerData.dpadLeftOn = !digitalRead(8);
-  // controllerData.dpadRightOn = !digitalRead(9);
-  // controllerData.l1On = !digitalRead(10);
-  // controllerData.r1On = !digitalRead(11);
-  // controllerData.selectOn = !digitalRead(12);
-  // controllerData.startOn = !digitalRead(A4);
-  // controllerData.homeOn = !digitalRead(A5);
-  
-  // Set the analog sticks
-  //  Since analogRead(pin) returns a 10 bit value,
-  //  we need to perform a bit shift operation to
-  //  lose the 2 least significant bits and get an
-  //  8 bit number that we can use  
-
-  // controllerData.leftStickX = analogRead(A0) >> 2;
-  // controllerData.leftStickY = analogRead(A1) >> 2;
-  // controllerData.rightStickX = analogRead(A2) >> 2;
-  // controllerData.rightStickY = analogRead(A3) >> 2;
-  
-  if ( usePPM ) {
-    // use AUX1 switch to press the triangle button
-    controllerData.triangleOn = rcValue[AUX1] > 1500;
-    
-    // use stick positions from TX instead of analogReads
-    controllerData.leftStickX = stickValue(rcValue[YAW]);
-    controllerData.leftStickY = stickValue(rcValue[THROTTLE]);
-    controllerData.rightStickX = stickValue(rcValue[ROLL]);
-    controllerData.rightStickY = stickValue(rcValue[PITCH]);
-  }
-  
-  // And return the data!
-  return controllerData;
+// set joystick data in HID descriptor
+void setControllerDataJoystick(){
+	Joystick.setXAxis(stickValue(rcValue[YAW]));
+	Joystick.setYAxis(stickValue(rcValue[THROTTLE]));
+	Joystick.setXAxisRotation(stickValue(rcValue[ROLL]));
+	Joystick.setYAxisRotation(stickValue(rcValue[PITCH]));
+	Joystick.setButton(0, rcValue[AUX1] > STICK_CENTER);
+	Joystick.setButton(1, rcValue[AUX2] > STICK_CENTER);
 }
 
-////////////////////////////////////////////////////////////////////////////////////
-
-// Convert a value in the range 1000-2000 to 0-255
+// Convert a value in the range of [Min Pulse - Max Pulse] to [0 - USB_STICK_VALUE_MAX]
 uint16_t stickValue(uint16_t rcVal) {
-  return constrain(rcVal - 1000, 0, 1000);
+	return constrain(
+		map(rcVal - MIN_PULSE_WIDTH, 
+			0, MAX_PULSE_WIDTH - MIN_PULSE_WIDTH, 
+			0, USB_STICK_VALUE_MAX
+		),
+		0, USB_STICK_VALUE_MAX
+	);
 }
 
-// Some adjustments I used because my cheapass TX has no trims.
-// If your TX has trims you will not need this and you can simply
-// return the 'diff' value unchanged.
 uint16_t adjust(uint16_t diff, uint8_t chan) {
-  /*switch (chan) {
-    case THROTTLE: return diff + 50;
-    case YAW:      return diff + 60;
-    case PITCH:    return diff + 60;
-    case ROLL:     return diff + 90;
-    case AUX1:     return diff + 10;
-  }*/
-  return diff;
+	// Here you can trim your rc values (e.g. if TX has no trims).
+	
+	// switch (chan) {
+	//   case THROTTLE: return diff + 50;
+	//   case YAW:      return diff + 60;
+	//   case PITCH:    return diff + 60;
+	//   case ROLL:     return diff + 90;
+	//   case AUX1:     return diff + 10;
+	// }
+
+	//convert to microseconds (because of timer prescaler usage)
+	return diff / TIMER_COUNT_DIVIDER;
 }
 
-// PPM interrupt routine courtesy of MultiWii source code
-void rxInt(void) {
-  uint16_t now, diff;
-  static uint16_t last = 0;
-  static uint8_t chan = 0;
+ISR(TIMER1_CAPT_vect) {
+	union twoBytes {
+		uint16_t word;
+		uint8_t  byte[2];
+	} timeValue;
 
-  now = micros();
-  sei();
-  diff = now - last;
-  last = now;
-  if(diff > 3000) {
-    chan = 0;
-  }
-  else {
-    if(900 < diff && diff < 2200 && chan < RC_CHANS) {
-      rcValue[chan] = adjust(diff,chan);
-    }
-    chan++;
-  }
+	uint16_t now, diff;
+	static uint16_t last = 0;
+	static uint8_t chan = 0;
+	
+	timeValue.byte[0] = ICR1L;    // grab captured timer value (low byte)
+	timeValue.byte[1] = ICR1H;    // grab captured timer value (high byte)
+
+	now = timeValue.word;
+	diff = now - last;
+	last = now;
+
+	//all numbers are microseconds multiplied by TIMER_COUNT_DIVIDER (as prescaler is set to 1/8 of 16 MHz)
+	if(diff > (NEWFRAME_PULSE_WIDTH * TIMER_COUNT_DIVIDER)) {
+		chan = 0;  // new data frame detected, start again
+	}
+	else {
+		if(diff > (MIN_PULSE_WIDTH * TIMER_COUNT_DIVIDER - THRESHOLD) 
+			&& diff < (MAX_PULSE_WIDTH * TIMER_COUNT_DIVIDER + THRESHOLD) 
+			&& chan < RC_CHANNELS_COUNT) 
+		{
+			rcValue[chan] = adjust(diff, chan); //store detected value
+		}
+		chan++; //no value detected within expected range, move to next channel
+	}
 }
-
